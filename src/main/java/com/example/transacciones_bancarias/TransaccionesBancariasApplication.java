e58c055c96e148b3953dab484aef0dc54148f41b
david morales <edavid.morales@udea.edu.co>
2024-01-31 14:58:28 -0600

    -creacion poryecto tansacciones

---
+++ src/main/java/com/example/transacciones_bancarias/TransaccionesBancariasApplication.java
@@ -0,0 +1,13 @@
+package com.example.transacciones_bancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransaccionesBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransaccionesBancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transacciones_bancarias/TransaccionesBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transacciones_bancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionesBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
